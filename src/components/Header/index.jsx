8a627b8fe47c891305c1c48d3db6418a5917a03e
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-03-17 17:00:55 -0300

    Migrando a aplicação de Class Componentes para Functional Components.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,24 @@
-import { Component } from 'react';
-import { FiPlusSquare } from 'react-icons/fi';
+import { FiPlusSquare } from "react-icons/fi";
 
-import { Container } from './styles';
-import Logo from '../../assets/logo.svg';
+import { Container } from "./styles";
+import Logo from "../../assets/logo.svg";
 
-class Header extends Component {
-  render() {
-    const { openModal } = this.props;
-
-    return (
-      <Container>
-        <header>
-          <img src={Logo} alt="GoRestaurant" />
-          <nav>
-            <div>
-              <button
-                type="button"
-                onClick={openModal}
-              >
-                <div className="text">Novo Prato</div>
-                <div className="icon">
-                  <FiPlusSquare size={24} />
-                </div>
-              </button>
-            </div>
-          </nav>
-        </header>
-      </Container>
-    )
-  }
-};
-
-export default Header;
+export function Header({ openModal }) {
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button type="button" onClick={openModal}>
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  );
+}
